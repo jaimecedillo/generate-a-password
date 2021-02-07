f4d40d30f19a5692f95945f0ac825dd4fd019d2d
jaime <jaime@jaimes-MacBook.local>
2021-02-06 20:26:54 -0700

    set up strings and prompts

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment code here
 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialCharacters = "~`! @#$%^&*()_-+={[}]|?";
+function generateRandomLetter() {
 
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return randomCharacter = upperCase[Math.floor(Math.random() * upperCase.length)]
+}
+var passwordLength
+
+prompt('How long would you like your password to be? Limit 8 to 128 characters long. Please enter number desired.');
+confirm('Would you like your password to have Uppercase letters?');
+confirm('Would you like your password to have Lowercase letters?');
+confirm('Would you like your password to have Numbers?');
+confirm('Would you like your password to have special characters?');
+alert("Please select at least one option");
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -14,4 +30,8 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.getElementById(generate).addEventListener("click", writePassword);
+
+function writePassword() {
+  document.getElementById("generate").innerHTML = password;
+}
